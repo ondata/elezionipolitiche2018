b5b6e6fcf64b9cace441b195895efe4632359e71
aborruso <aborruso@gmail.com>
2018-03-05 09:27:52 +0100

    aggiunti dati su scrutini Senato e normalizzato struttura

--- README.md
+++ README.md
@@ -19,8 +19,16 @@ Degli script per scaricare i dati sulle elezioni politiche del 4 marzo 2018 in I
 - votanti per Comune Senato > [votantiSIComuni.csv](./dati/votantiSIComuni.csv)
 - anagrafica con i codici delle province > [anagraficaProvince.csv](./dati/anagraficaProvince.csv)
 
+**NOTA BENE**: manca la sezione Estero
+
 #### Scrutini
 
-- scrutini Camera per Circoscrizione, Camera: [scrutiniCI_c.csv](./dati/scrutiniCI_c.csv)
-- scrutini Camera per Colleggio Plurinominale, Camera: [scrutiniCI_c.csv](./dati/scrutiniCI_p.csv)
-- scrutini Camera per Colleggio Uninominale, Camera: [scrutiniCI_c.csv](./dati/scrutiniCI_u.csv)
+- scrutini Camera per Circoscrizione: [scrutiniCI_c.csv](./dati/scrutiniCI_c.csv)
+- scrutini Camera per Collegio Plurinominale: [scrutiniCI_c.csv](./dati/scrutiniCI_p.csv)
+- scrutini Camera per Collegio Uninominale: [scrutiniCI_c.csv](./dati/scrutiniCI_u.csv)
+- scrutini Senato per Circoscrizione: [scrutiniCI_c.csv](./dati/scrutiniSI_c.csv)
+- scrutini Senato per Collegio Plurinominale: [scrutiniSI_c.csv](./dati/scrutiniSI_p.csv)
+- scrutini Senato per Collegio Uninominale: [scrutiniSI_c.csv](./dati/scrutiniSI_u.csv)
+
+
+**NOTA BENE**: manca la sezione Estero
--- dwScrutini.sh
+++ dwScrutini.sh
@@ -22,10 +22,10 @@ csvcut <./camera_geopolitico_italia.csv -c 1 >./camera_geopolitico_italia_tmp.cs
 # estraggo i codici dei Comuni
 ack '^...[^0](?!0000000)' ./camera_geopolitico_italia_tmp.csv >./scrutini/comuni.csv
 
-# estraggo i codici dei colleggi uninominali
+# estraggo i codici dei collegi uninominali
 grep <./camera_geopolitico_italia_tmp.csv -E '...[^0]0000000' >./scrutini/colleggiUninominali.csv
 
-# estraggo i codici dei colleggi plurinominali
+# estraggo i codici dei collegi plurinominali
 grep <./camera_geopolitico_italia_tmp.csv -E '^..[^0]00000000' >./scrutini/colleggiPlurinominali.csv
 
 # estraggo i codici delle circoscrizioni
@@ -43,14 +43,14 @@ while read p; do
 		-H 'postman-token: b8b67cdf-9521-4e07-2e63-99b28b20c2be' \
 		-H 'referer: http://elezioni.interno.gov.it/camera/scrutini/20180304/scrutiniCI'"$p"'' \
 		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
-		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{codice:"'"$p"'",tipo:"circoscrizione",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_c"$p".json
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Camera",codice:"'"$p"'",tipo:"circoscrizione",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_c"$p".json
 done <./scrutini/circoscrizioni.csv
 
 jq -s add ./scrutini/scrutiniCI_c*.json >./scrutini/scrutiniCI_c.json
 
 in2csv <./scrutini/scrutiniCI_c.json -I -f json >./scrutiniCI_c.csv
 
-# scarico i dettagli sugli scrutini alla Camera per Colleggio Plurinominale
+# scarico i dettagli sugli scrutini alla Camera per Collegio Plurinominale
 while read p; do
 	curl -X GET \
 		http://elezioni.interno.gov.it/politiche/camera20180304/scrutiniCI"$p" \
@@ -62,14 +62,14 @@ while read p; do
 		-H 'postman-token: b8b67cdf-9521-4e07-2e63-99b28b20c2be' \
 		-H 'referer: http://elezioni.interno.gov.it/camera/scrutini/20180304/scrutiniCI'"$p"'' \
 		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
-		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{codice:"'"$p"'",tipo:"colleggioPlurinominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_p"$p".json
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Camera",codice:"'"$p"'",tipo:"colleggioPlurinominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_p"$p".json
 done <./scrutini/colleggiPlurinominali.csv
 
 jq -s add ./scrutini/scrutiniCI_p*.json >./scrutini/scrutiniCI_p.json
 
 in2csv <./scrutini/scrutiniCI_p.json -I -f json >./scrutiniCI_p.csv
 
-# scarico i dettagli sugli scrutini alla Camera per Colleggio Uninominale
+# scarico i dettagli sugli scrutini alla Camera per Collegio Uninominale
 while read p; do
 	curl -X GET \
 		http://elezioni.interno.gov.it/politiche/camera20180304/scrutiniCI"$p" \
@@ -81,12 +81,90 @@ while read p; do
 		-H 'postman-token: b8b67cdf-9521-4e07-2e63-99b28b20c2be' \
 		-H 'referer: http://elezioni.interno.gov.it/camera/scrutini/20180304/scrutiniCI'"$p"'' \
 		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
-		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{codice:"'"$p"'",tipo:"colleggioUninominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_u"$p".json
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Camera",codice:"'"$p"'",tipo:"colleggioUninominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniCI_u"$p".json
 done <./scrutini/colleggiUninominali.csv
 
 jq -s add ./scrutini/scrutiniCI_u*.json >./scrutini/scrutiniCI_u.json
 
 in2csv <./scrutini/scrutiniCI_u.json -I -f json >./scrutiniCI_u.csv
+
+### SENATO ###
+
+curl -sL "http://elezioni.interno.gov.it/assets/enti/senato_geopolitico_italia.json" | jq . >./senato_geopolitico_italia.json
+
+# li converto in CSV
+jq '[.enti[]]' ./senato_geopolitico_italia.json | in2csv -I -f json >./senato_geopolitico_italia.csv
+
+csvcut <./senato_geopolitico_italia.csv -c 1 >./senato_geopolitico_italia_tmp.csv
+
+# estraggo i codici dei Comuni
+ack '^...[^0](?!0000000)' ./senato_geopolitico_italia_tmp.csv >./scrutini/comuniSI.csv
+
+# estraggo i codici dei collegi uninominali
+grep <./senato_geopolitico_italia_tmp.csv -E '...[^0]0000000' >./scrutini/colleggiUninominaliSI.csv
+
+# estraggo i codici dei collegi plurinominali
+grep <./senato_geopolitico_italia_tmp.csv -E '^..[^0]00000000' >./scrutini/colleggiPlurinominaliSI.csv
+
+# estraggo i codici delle circoscrizioni
+grep <./senato_geopolitico_italia_tmp.csv -E '^.[^0]0{9}' >./scrutini/circoscrizioniSI.csv
+
+# scarico i dettagli sugli scrutini al Senato per Circoscrizione
+while read p; do
+	curl -X GET \
+		http://elezioni.interno.gov.it/politiche/senato20180304/scrutiniSI"$p" \
+		-H 'accept: application/json, text/javascript, */*; q=0.01' \
+		-H 'accept-encoding: gzip, deflate' \
+		-H 'accept-language: it,en-US;q=0.9,en;q=0.8' \
+		-H 'cache-control: no-cache' \
+		-H 'content-type: application/json' \
+		-H 'postman-token: d7765423-a0a9-6a75-eafb-965ef2e8aaf2' \
+		-H 'referer: http://elezioni.interno.gov.it/senato/scrutini/20180304/scrutiniSI'"$p"'' \
+		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Senato",codice:"'"$p"'",tipo:"circoscrizione",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniSI_c"$p".json
+done <./scrutini/circoscrizioniSI.csv
+
+jq -s add ./scrutini/scrutiniSI_c*.json >./scrutini/scrutiniSI_c.json
+
+in2csv <./scrutini/scrutiniSI_c.json -I -f json >./scrutiniSI_c.csv
+
+# scarico i dettagli sugli scrutini al Senato per Collegio Plurinominale
+while read p; do
+	curl -X GET \
+		http://elezioni.interno.gov.it/politiche/senato20180304/scrutiniSI"$p" \
+		-H 'accept: application/json, text/javascript, */*; q=0.01' \
+		-H 'accept-encoding: gzip, deflate' \
+		-H 'accept-language: it,en-US;q=0.9,en;q=0.8' \
+		-H 'cache-control: no-cache' \
+		-H 'content-type: application/json' \
+		-H 'postman-token: d7765423-a0a9-6a75-eafb-965ef2e8aaf2' \
+		-H 'referer: http://elezioni.interno.gov.it/senato/scrutini/20180304/scrutiniSI'"$p"'' \
+		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Senato",codice:"'"$p"'",tipo:"colleggioPlurinominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniSI_p"$p".json
+done <./scrutini/colleggiPlurinominaliSI.csv
+
+jq -s add ./scrutini/scrutiniSI_p*.json >./scrutini/scrutiniSI_p.json
+
+in2csv <./scrutini/scrutiniSI_p.json -I -f json >./scrutiniSI_p.csv
+
+# scarico i dettagli sugli scrutini al Senato per Collegio Uninominale
+while read p; do
+	curl -X GET \
+		http://elezioni.interno.gov.it/politiche/senato20180304/scrutiniSI"$p" \
+		-H 'accept: application/json, text/javascript, */*; q=0.01' \
+		-H 'accept-encoding: gzip, deflate' \
+		-H 'accept-language: it,en-US;q=0.9,en;q=0.8' \
+		-H 'cache-control: no-cache' \
+		-H 'content-type: application/json' \
+		-H 'postman-token: d7765423-a0a9-6a75-eafb-965ef2e8aaf2' \
+		-H 'referer: http://elezioni.interno.gov.it/senato/scrutini/20180304/scrutiniSI'"$p"'' \
+		-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.106 Safari/537.36' \
+		-H 'x-requested-with: XMLHttpRequest' | jq '[.righe[]|{assemblea:"Senato",codice:"'"$p"'",tipo:"colleggioUninominale",tipo_riga,cand_descr_riga,img_lista,descr_lista,link_cand_lista,voti,perc,eletto,perc_voti_liste,cifra_el,perc_cifra_el,seggi}]' >./scrutini/scrutiniSI_u"$p".json
+done <./scrutini/colleggiUninominaliSI.csv
+
+jq -s add ./scrutini/scrutiniSI_u*.json >./scrutini/scrutiniSI_u.json
+
+in2csv <./scrutini/scrutiniSI_u.json -I -f json >./scrutiniSI_u.csv
 
 mv ./*.json ./dati
 mv ./*.csv ./dati
