e493e2165b272dbc3401cc0fee70f9d0d0a663c0
aborruso <aborruso@gmail.com>
2018-03-08 08:24:49 +0100

    aggiornate province votanti

--- dwVotanti.sh
+++ dwVotanti.sh
@@ -17,7 +17,7 @@ mkdir -p ./tmp
 jq '[.enti[]]' ./dati/camerasenato_territoriale_italia.json | in2csv -I -f json >./dati/camerasenato_territoriale_italia.csv
 
 # estraggo la lista dei codici delle province
-csvcut -c 1 ./dati/camerasenato_territoriale_italia.csv | grep -E '[^0]0000$' >./tmp/campioneProvince.csv
+csvcut -c 1 ./dati/camerasenato_territoriale_italia.csv | grep -E '^.*0000$' | grep -v -E '.*000000$' >./tmp/campioneProvince.csv
 
 # estraggo l'anagrafica dei codici delle province
 csvgrep -c "id" -r "[^0]0000$" ./dati/camerasenato_territoriale_italia.csv >./anagraficaProvince.csv
